29d7b4a843fc77582df218b906461a3f8473fa3b
Bryan Rabanales <brabanales-2018466@kinal.edu.gt>
2023-01-20 08:58:42 -0600

    Primer commit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function saludar() {
+
+    let valor1 = document.getElementById("num1").value;
+    let valor2 = document.getElementById("num2").value;
+    
+    let resultado = parseInt(valor1) + parseInt(valor2);
+    
+    console.log('Saludos');
+    console.log(resultado);
+
+}
